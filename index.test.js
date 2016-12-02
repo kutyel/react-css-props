da9b6a7512f6cedfb80b2c895af6bbb5555160a7
kutyel <flaviocorpa@gmail.com>
2016-12-02 10:30:17 +0100

    test(mapper): make sure adding a custom mapper function works as expected

--- index.test.js
+++ index.test.js
@@ -2,26 +2,39 @@ import cssProps from './index';
 
 // Mock imported theme from CSS Modules
 const theme = {
-    button: 'Button__button___3_Ozh',
-    primary: 'Button__primary___zYyzg'
+    'icon': 'Icon__icon___66GYG',
+    'button': 'Button__button___3_Ozh',
+    'primary': 'Button__primary___zYyzg',
+    'icon-save': 'Icon__icon-save___e4uoa'
 };
-
-// Prepare the clases according to given theme
-const toCSS = cssProps(theme);
 
 describe('react-css-props', () => {
 
-    test('should always return an array', () => {
-        expect(toCSS({})).toEqual([]);
+    test('should always return a function', () => {
+        expect(typeof cssProps({})).toBe('function');
+        expect(typeof cssProps(theme)).toBe('function');
+    });
+
+    test('generated function should always return an array', () => {
+        expect(cssProps(theme)({})).toEqual([]);
     })
 
     test('should return an array with the specified default class', () => {
-        const className = 'button';
-        expect(toCSS({}, className)).toEqual([theme[className]]);
+        expect(cssProps(theme)({}, 'button')).toEqual([theme.button]);
     });
 
     test('should have `undefined` when not finding the default class', () => {
-        expect(toCSS({}, 'default')[0]).toBeUndefined();
+        expect(cssProps(theme)({}, 'unexisting')).toEqual([undefined]);
+    });
+
+    test('should turn props into existing classNames', () => {
+        expect(cssProps(theme)({ icon: true })).toEqual([theme.icon]);
+    });
+
+    test('should use the mapper if provided', () => {
+        expect(
+            cssProps(theme, type => `icon-${type}`)({ save: true })
+        ).toEqual([theme['icon-save']]);
     });
 
 });
