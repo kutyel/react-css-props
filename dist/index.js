6ef63ccd3ebddf8b7329777226571d9de177edc3
kutyel <flaviocorpa@gmail.com>
2016-12-03 19:54:26 +0100

    chore(dist): Add latest generated build file (es5)

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,9 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = function (theme, mapper) {
     return function (props, defaultClass) {
         if (Object.keys(props).length) {
-            return Object.keys(props).map(function (className) {
+            return Object.keys(props).filter(function (prop) {
+                return !!props[prop];
+            }).map(function (className) {
                 return mapper ? theme[mapper(className)] : theme[className];
             });
         } else {
